a84420bf8a167f137213a24ed79cce878cdb8912
DarkTXYZ <pawaretmontfort@gmail.com>
2022-09-23 15:46:22 +0700

    initialize assembler code

--- Assembler/assembler.py
+++ Assembler/assembler.py
@@ -1,3 +1,6 @@
 class Assembler :
-    def __init__(self) -> None:
+    def __init__(self,label,op,f0,f1,f2) -> None:
         pass
+
+    def returnMachineCode() :
+        return 'fuck'
--- Assembler/main.py
+++ Assembler/main.py
@@ -1,3 +1,28 @@
 input = open('Assembler\input.txt' , 'r')
 
 print(input.readlines())
+
+# a = Assembler('','','','','')
+
+# a.returnMachineCode()
+# instruction = {
+#     'label' : '' ,
+#     'op' : '' ,
+#     'rs1' : '' ,
+#     'rs2' : '' ,
+#     'imm' : '' 
+# }
+
+a = [1,2,3]
+
+if 4 in a :
+    print('yes')
+
+for i in range(10) :
+    print(i)
+
+# for loop เก็บ label 
+# for loop วนอ่านคำสั่ง
+    # คำสั่งมี label , instruction , rs1 , ...
+    # โยนข้อมูลให้ Assembler -> machine code
+    # เอา machine code มา แสดง
