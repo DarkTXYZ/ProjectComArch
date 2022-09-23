392576c7a7e5138b6b50ecbe95d01eb4f5b97d59
majespectre00 <76035408+majespectre00@users.noreply.github.com>
2022-09-23 23:04:54 +0700

    spliteverything

--- Assembler/main.py
+++ Assembler/main.py
@@ -1,3 +1,8 @@
+from textwrap import fill
+
+from numpy import append
+
+
 input = open('Assembler\input.txt' , 'r')
 # print(input.readlines())
 readin = input.readlines()
@@ -15,17 +20,39 @@ readin = input.readlines()
 #     'imm' : '' 
 # }
 a = {}
-b = ["add","nand","lw","sw","beq","jalr","halt","noop"]
+b = ["add","nand","lw","sw","beq","jalr","halt","noop","halt\n","noop\n"]
+specialOP = ["halt","noop",".fill","halt\n","noop\n"]
 s= 0
+allLines =[]
 
 for i in readin:
-    spl = i.split(" ")[0] 
-    # print(i)
-    if (spl not in b) and i != "noop\n": 
-        a.update({spl:s})
-        print(spl)
-    print(s)
+    operation = []
+    line = i.split(" ")
+    isLabel = 0
+    if (line[0] not in b) and i != "noop\n": 
+        a.update({line[0]:s})
+        isLabel = 1
+    # print(line)
+    # print(isLabel)
+    # print(line[isLabel])
+    if(line[isLabel] not in specialOP):
+        for j in range (4):
+            it = j
+            operation.append(line[it+isLabel])
+    elif line[isLabel] == ".fill" :
+        operation.append(line[isLabel])
+        operation.append(line[isLabel+1])
+    else: operation.append(line[isLabel])
     s+=1
+    # print(operation)
+    allLines.append(operation)
+# print(allLines)
+print(a)
+for i in allLines:
+    print(i)
+# for i in allLines:
+#     print(i)
+    
 # for loop เก็บ label 
 # for loop วนอ่านคำสั่ง
     # คำสั่งมี label , instruction , rs1 , ...
