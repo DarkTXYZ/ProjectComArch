a7ad436abe895d52b156a2d40287737e7c193630
rbzgamer <kuro123520@hotmail.com>
2022-09-24 19:25:37 +0700

    add some comment and finish I-type & O-type part

--- Assembler/assembler.py
+++ Assembler/assembler.py
@@ -20,14 +20,23 @@ def Assembler(inst):
 
     if op == 'add' or op == 'nand':
         return R_type(op, f0, f1, f2)
+    elif op == 'lw' or op == 'sw' or op == 'beq':
+        return I_type(op, f0, f1, f2)
     elif op == 'jalr':
         return J_type(f0, f1)
+    elif op == 'halt' or op == 'noop':
+        return O_type(op, f0, f1, f2)
     elif op == '.fill':
         return f0
     else:
         return 'wrong operation'
 
-
+# R-type instructions (add, nand)
+#                Bits 24-22 opcode
+#                Bits 21-19 reg A (rs)
+#                Bits 18-16 res B (rt)
+#                Bits 15-3 ไม่ใช้ (ควรตั้งไว้ที่ 0)
+#                Bits 2-0  destReg (rd)
 def R_type(op, f0, f1, f2):
     bit = 0
     if op == 'add':
@@ -47,11 +56,44 @@ def R_type(op, f0, f1, f2):
 
     return bit
 
-
+# I-type instructions (lw, sw, beq)
+#                Bits 24-22 opcode
+#                Bits 21-19 reg A (rs)
+#                Bits 18-16 reg B (rt)
+#                Bits 15-0 offsetField (เลข16-bit และเป็น 2’s complement  โดยอยู่ในช่วง –32768 ถึง 32767)
 def I_type(op, f0, f1, f2):
-    pass
+    bit = 0
+    if op == 'lw':
+        bit24_22 = 1 << 23
+        bit21_19 = int(f0) << 19
+        bit18_16 = int(f1) << 16
+        bit15_0 = int(f2)
+        
+        bit = bit24_22 + bit21_19 + bit18_16 + bit15_0
+        
+    elif op == 'sw':
+        bit24_22 = 3 << 22
+        bit21_19 = int(f0) << 19
+        bit18_16 = int(f1) << 16
+        bit15_0 = int(f2)
+        
+        bit = bit24_22 + bit21_19 + bit18_16 + bit15_0
+        
+    elif op == 'beq':
+        bit24_22 = 1 << 24
+        bit21_19 = int(f0) << 19
+        bit18_16 = int(f1) << 16
+        bit15_0 = int(f2)
 
+        bit = bit24_22 + bit21_19 + bit18_16 + bit15_0
+        
+    return bit
 
+# J-Type instructions (jalr)
+#                Bits 24-22 opcode
+#                Bits 21-19 reg A (rs)
+#                Bits 18-16 reg B (rd)
+#                Bits 15-0 ไม่ใช้ (ควรตั้งไว้ที่ 0)
 def J_type(f0, f1):
     bit24_22 = 5 << 22
     bit21_19 = int(f0) << 19
@@ -61,7 +103,16 @@ def J_type(f0, f1):
 
     return bit
 
-
+# O-type instructions (halt, noop)
+#                Bits 24-22 opcode
+#                Bits 21-0 ไม่ใช้ (ควรตั้งไว้ที่ 0)
 def O_type(op):
-    pass
+    bit = 0
+    if op == 'halt':
+        bit = 3 << 23
+
+    elif op == 'noop':
+        bit = 7 << 22
+    
+    return bit
     
