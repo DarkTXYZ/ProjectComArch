26c9b101057bf741f005293728e42bac3ad03d26
majespectre00 <76035408+majespectre00@users.noreply.github.com>
2022-09-26 17:43:49 +0700

    easier loop with .isnumeric() shinanigans

--- Assembler/main.py
+++ Assembler/main.py
@@ -1,84 +1,123 @@
-import imp
-from assembler import Assembler
+# from assembler import Assembler
 
 input = open('Assembler\input.txt', 'r')
 output = open('Assembler\output.txt', "w")
-
-readin = input.readlines()
-
-
-# a = Assembler('','','','','')
-
-# a.returnMachineCode()
-# instruction = {
-#     'label' : '' ,
-#     'op' : '' ,
-#     'rs1' : '' ,
-#     'rs2' : '' ,
-#     'imm' : ''
-# }
-a = {}
-b = ["add", "nand", "lw", "sw", "beq", "jalr", "halt", "noop"]
+readIn = input.readlines()
+labelMapping = {}
+operations = ["add", "nand", "lw", "sw", "beq", "jalr", "halt", "noop",".fill"]
 specialOP = ["halt", "noop", ".fill"]
-s = 0
+branchOP = ["lw","sw","beq"]
+count = 0
 allLines = []
-for i in readin:
-    line = i.split(" ")
-    line[len(line)-1] = line[len(line)-1].replace("\n","")
-    if (line[0] not in b): 
-        a.update({line[0]:s})
-    s+=1 
-print(a) 
-s=0
-for i in readin:
-    operation = []
+
+#Mapping Loop
+for i in readIn:
     line = i.split(" ")
     line[len(line)-1] = line[len(line)-1].replace("\n", "")
-    # print(line)
-    # print(line[len(line)-1])
+    if (line[0] not in operations and line[0] not in labelMapping):
+        if(len(line)==1):
+            print("wrong opcode")
+            exit(1)
+        labelMapping.update({line[0]: count})
+    count += 1
+print(labelMapping)
+count = 0
 
+#Input Loop
+for i in readIn:
+    operation=[]
+    line = i.split(" ")
+    line[len(line)-1] = line[len(line)-1].replace("\n", "")
     isLabel = 0
-    if (line[0] not in b) and i != "noop\n":
-        isLabel = 1
-    # print(line)
-    # print(isLabel)
-    # print(line[isLabel])
-    if(line[isLabel] not in specialOP and line[isLabel] not in b):
-        print("wrong input")
-        exit()
+    if(line[0] in labelMapping): isLabel = 1
+    print(line)
+    if(line[isLabel] not in operations):
+        print("wrong opcode")
+        exit(1)
+    if(line[isLabel] in branchOP and line[isLabel+3].isnumeric()):
+        lowest = -32768
+        if(int(line[3+isLabel]) < lowest  or int(line[3+isLabel])>32767):
+            print("too greedy")
+            exit(1) 
     if(line[isLabel] not in specialOP):
-        for j in range(4):
-            it = j
-            if(j == 3 and line[it+isLabel] in a):
-                # print(a[line[it+isLabel]])
-                if(line[isLabel] == 'beq'):
-                    line[it+isLabel] = str(a[line[it+isLabel]]-s-1)
-                else:
-                    line[it+isLabel] =str(a[line[it+isLabel]])
-            
-            operation.append(line[it+isLabel])
+        operation.append(line[isLabel])
+        operation.append(line[isLabel+1])
+        operation.append(line[isLabel+2])
+        if(line[isLabel+3].isnumeric()) : operation.append(line[isLabel+3])
+        else:
+            if(line[isLabel+3] in labelMapping):
+                if(line[isLabel] == 'beq'): operation.append(str(labelMapping[line[isLabel+3]]-count-1))
+                else:operation.append(str(labelMapping[line[isLabel+3]]))
+            else:
+                print("undefined label")
+                exit(1)
     elif line[isLabel] == ".fill":
         operation.append(line[isLabel])
-
-        if(line[isLabel+1] in a):
-            line[isLabel+1] = str(a[line[isLabel+1]])
-       
-        operation.append(line[isLabel+1])
-    else:
+        print(line[isLabel+1])
+        if(line[isLabel+1].isnumeric()): operation.append(line[isLabel+1])
+        else:
+            if(line[isLabel+1] in labelMapping): operation.append(str(labelMapping[line[isLabel+1]]))
+            else:
+                print("undefined label")
+                exit(1)
+    elif line[isLabel] == "halt" or line[isLabel] == "noop":
         operation.append(line[isLabel])
-    # print(operation)
+    count+=1
     allLines.append(operation)
-    s+=1
-# print(allLines)
-# print(a)
+
+
+
+
+
+
+
+
+
+
+# for i in readIn:
+#     operation = []
+#     line = i.split(" ")
+#     line[len(line)-1] = line[len(line)-1].replace("\n", "")
+#     isLabel = 0
+#     if (line[0] not in operations) and i != "noop\n":
+#         isLabel = 1
+#     if (line[isLabel] not in specialOP and line[isLabel] not in operations):
+#         print("wrong input")
+#         exit(1)
+  
+#     if (line[isLabel] not in specialOP):
+#         for j in range(4):
+#             if(j==3 and not line[3+isLabel].isnumeric()):
+#                 if (line[3+isLabel] in labelMapping):
+#                     if (line[isLabel] == 'beq'):
+#                         line[3+isLabel] = str(labelMapping[line[3+isLabel]]-count-1)
+#                     else:
+#                         line[3+isLabel] = str(labelMapping[line[3+isLabel]])
+#                 else: 
+#                     print("wrong label")
+#                     exit(1)
+#             operation.append(line[j+isLabel])
+#     elif line[isLabel] == ".fill":
+#         operation.append(line[isLabel])
+#         if (line[isLabel+1] in labelMapping):
+#             line[isLabel+1] = str(labelMapping[line[isLabel+1]])
+#         if(line[isLabel+1] not in labelMapping and not line[isLabel+1].isnumeric()):
+#             print("undefine label")
+#             exit(1)
+#         operation.append(line[isLabel+1])
+#     else:
+#         operation.append(line[isLabel])
+#     if(line[isLabel] in branchOP):
+#         lowest = -32768
+#         if(int(line[3+isLabel]) < lowest  or int(line[3+isLabel])>32767):
+#             print("too greedy")
+#             exit(1) 
+#     allLines.append(operation)
+#     count += 1
+#Correction Loop
 
 for i in allLines:
-    machineCode = Assembler(i)
-    print(i , machineCode)
-    output.write(str(machineCode) + '\n')
-
-# for loop เก็บ label
-# for loop วนอ่านคำสั่ง
-    # คำสั่งมี label , instruction , rs1 , ...
-    # โยนข้อมูลให้ Assembler -> machine code
-    # เอา machine code มา แสดง
+    # machineCode = Assembler(i)
+    # print(i, machineCode)
+    # output.write(str(machineCode) + '\n')
+    print(i)
