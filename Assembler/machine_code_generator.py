f5a9c92b3d625f613a54a0d3a650063642e7ac66
majespectre00 <76035408+majespectre00@users.noreply.github.com>
2022-10-18 21:00:25 +0700

    more case detected

--- Assembler/machine_code_generator.py
+++ Assembler/machine_code_generator.py
@@ -1,3 +1,6 @@
+from atexit import register
+
+
 def MachineCodeGenerator() : 
     from Assembler.assembler import Assembler
 
@@ -31,11 +34,24 @@ def MachineCodeGenerator() :
     for i in readIn:
         line = i.split()
         line[len(line)-1] = line[len(line)-1].replace("\n", "")
+        #check if 
         if (line[0] not in operations):
+            if(len(line) == 1):
+                    print("wrong opcode: probably input only a label no opeation")
+                    exit(1)
+            if(line[1] not in operations):
+                print("wrong syntax: maybe inputing 2 label or wrong opcode")
+                exit(1)
             if(line[0] not in labelMapping):
+                #check if the line only has a label
                 if(len(line) == 1):
                     print("wrong opcode: probably input only a label no opeation")
                     exit(1)
+                #check if the first char is numberic
+                elif(numcheck(line[0][0])):
+                    print("first char is numberic")
+                    exit(1)
+                #check if the label is longer than 6 char
                 elif(len(line[0])>6):
                     print("label exceeds 6 character")
                     exit(1)
@@ -43,7 +59,9 @@ def MachineCodeGenerator() :
                     labelMapping.update({line[0]: count})
             else:
                 print("label already define: "+line[0])
+                exit(1)
         count += 1
+
     count = 0
 
     # Input Loop
@@ -59,9 +77,6 @@ def MachineCodeGenerator() :
         # without label      opcode      field1
         if(line[0] in labelMapping):
             isLabel = 1
-        if(isLabel == 1 and line[1] in operations):
-            print("label cannot be operation")
-            exit(1)
         # check if opcode is known in requirement
         if(line[isLabel] not in operations):
             print("wrong opcode")
@@ -70,11 +85,16 @@ def MachineCodeGenerator() :
         if(line[isLabel] in branchOP and numcheck(line[isLabel+3])):
             lowest = -32768
             if(int(line[3+isLabel]) < lowest or int(line[3+isLabel]) > 32767):
-                print("too greedy")
+                print("offsetfiled is out of range ")
                 exit(1)
         # spliting sessions
         # 3 field operation ex add r1 r2 r3
         if(line[isLabel] not in specialOP):
+            if(not numcheck(line[isLabel+1]) and not numcheck(line[isLabel+2]) ):
+                print("register cannot be symbolic")
+                exit(1)
+            if(int(line[isLabel+1]) > 7  or int(line[isLabel+2]) >7):
+                print("register out of range")
             operation.append(line[isLabel])
             operation.append(line[isLabel+1])
             operation.append(line[isLabel+2])
@@ -97,7 +117,14 @@ def MachineCodeGenerator() :
             operation.append(line[isLabel])
             # check for last field label
             if(numcheck(line[isLabel+1])):
-                operation.append(line[isLabel+1])
+                if(int(line[isLabel+1]) >2147483647):
+                    print("number is out of range .fill")
+                    exit(1)
+                elif(int(line[isLabel+1]) < -2147483648):
+                    print("number is out of range .fill")
+                    exit(1)
+                else:
+                    operation.append(line[isLabel+1])
             else:
                 if(line[isLabel+1] in labelMapping):
                     operation.append(str(labelMapping[line[isLabel+1]]))
@@ -107,6 +134,11 @@ def MachineCodeGenerator() :
         # 2 field operation
         elif line[isLabel] == "jalr":
             operation.append(line[isLabel])
+            if(not numcheck(line[isLabel+1])):
+                print("register cannot be symbolic")
+                exit(1)
+            if(int(line[isLabel+1]) > 7):
+                print("register out of range")
             operation.append(line[isLabel+1])
             # check label
             if(numcheck(line[isLabel+2])):
@@ -125,6 +157,8 @@ def MachineCodeGenerator() :
 
     for i in allLines:
         machineCode = Assembler(i)
+        print(i)
+        print(machineCode)
         output.write(str(machineCode) + '\n')
         
     input.close()
